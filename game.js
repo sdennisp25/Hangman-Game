15204ede906e47d9da09954b0f57d194c4239b1f
Dennis The Menace <sdennisp25@hotmail.com>
2018-08-13 22:55:20 -0600

    got the code to work with a startGame function to restart the game after the conditions have been met

--- game.js
+++ game.js
@@ -1,39 +1,41 @@
 //GUESS THE FOOTBALL TEAM!
 
-
+console.clear();
 var footBall = ['raiders', 'seahawks', 'dolphins', 'patriots', 'cardinals', 'chiefs'];
 var guessesLeft = 21;
 var wins = 1;
 var losses = 1;
-var blankLines = [];
+var blankLines;
 var guessedLetters = [];
-
-var computerGuess = footBall[Math.floor(Math.random() * footBall.length)];
-console.log(computerGuess);
+var computerGuess;
 
 function startGame() {
+	guessesLeft = 21;
+	blankLines = [];
+	guessedLetters = [];
+	computerGuess = footBall[Math.floor(Math.random() * footBall.length)];
+console.log(computerGuess);
 	for (var i = 0; i < computerGuess.length; i++) {
-		blankLines.push('_ ');
-		console.log(blankLines);
-		document.getElementById("blankLines").innerHTML = blankLines;
+		blankLines.push('_');
+		document.getElementById("blankLines").innerHTML = blankLines.join(' ');
 	}
 }
 
+
 function resetGame() {
-	var guessesLeft = 20;
-	var guessedLetters = [];
-	var computerGuess = footBall[Math.floor(Math.random() * footBall.length)];
-	console.log(computerGuess);
+
+	// document.getElementById("blankLines").innerHTML = blankLines.join('_ ');
+	document.getElementById("lettersGuessed").innerHTML = guessedLetters;
 }
 
 startGame();
-
+resetGame();
 document.onkeyup = function (event) {
 	var userGuess = event.key;
 	console.log(userGuess);
 	guessedLetters.push(userGuess);
 	document.getElementById("lettersGuessed").innerHTML = guessedLetters;
-	
+
 	// will help us locate the userGuesses letter within the computer random array
 	if (computerGuess.indexOf(userGuess) > -1) {
 		for (var i = 0; i < computerGuess.length; i++) {
@@ -41,29 +43,32 @@ document.onkeyup = function (event) {
 				blankLines[i] = userGuess;
 			}
 		}
-		console.log(typeof blankLines)
-		document.getElementById("blankLines").innerHTML = blankLines;
+		//will replace blankLine with userGuess
+		// console.log(typeof blankLines)
+		document.getElementById("blankLines").innerHTML = blankLines.join(' ');
 	}
+	
 
 
 	if (computerGuess === blankLines.join('')) {
 		alert("you win!");
-		document.getElementById("win").innerHTML = win; win++;
+		document.getElementById("wins").innerHTML = wins; wins++;
+		startGame();
 	}
-	
+
 	else {
 		guessesLeft--;
 		document.getElementById("guessesLeft").innerHTML = guessesLeft;
 	}
-	
+
 	if (guessesLeft === 0) {
 		alert("game over");
 		document.getElementById("losses").innerHTML = losses; losses++;
-		resetGame();
+		startGame();
 	}
-	
-	
-	
+
+
+
 }
 
 
